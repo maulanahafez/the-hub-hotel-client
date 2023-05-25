8cf439317ad89f58706cf24336070e9603149ed4
maulanahafez <maulanahafez00@gmail.com>
2023-05-25 22:36:56 +0700

    0.5
    client done
    dashboard landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
       },
       boxShadow: {
         "dark-blue": "0px 0px 15px -2px rgba(8,47,73,0.75)",
+        "dark-custom": "0px 0px 2px -1px rgba(0,0,0,1)",
       },
     },
   },
